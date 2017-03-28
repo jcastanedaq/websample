b48e3ed9cdf973fa02abffe62d0bde03f1d83a4b
JORGE CASTAÑEDA <jcastaneda@iMac-7.local>
2017-03-27 21:03:13 -0600

    Initial Commit

---
+++ Templates/plantilla.dwt.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>Graph Project</title>
+<!-- TemplateEndEditable -->
+<!-- Bootstrap core CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/bootstrap-theme.min.css" rel="stylesheet">
+
+<!-- Custom styles for this template -->
+<link href="css/sticky-footer-navbar.css" rel="stylesheet">
+<link href="../css/estilos.css" rel="stylesheet">
+<link href="jquery-ui-1.11.1.custom/jquery-ui.css" rel="stylesheet">
+<!-- Bootstrap core JavaScript
+    ================================================== --> 
+<!-- Placed at the end of the document so the pages load faster --> 
+<script src="js/jquery.min.js"></script> 
+<script src="js/bootstrap.min.js"></script>
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+</head>
+
+<body>
+
+<!-- Fixed navbar -->
+
+
+<!-- Begin page content --><!-- TemplateBeginEditable name="contenido" -->contenido<!-- TemplateEndEditable -->
+
+<div class="footer">
+  <div class="container">
+    <p class="text-muted text-center">TamTI 2015.</p>
+  </div>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/plantilla.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>En construccion</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap core CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/bootstrap-theme.min.css" rel="stylesheet">
+
+<!-- Custom styles for this template -->
+<link href="css/sticky-footer-navbar.css" rel="stylesheet">
+<link href="css/estilos.css" rel="stylesheet">
+<link href="jquery-ui-1.11.1.custom/jquery-ui.css" rel="stylesheet">
+<!-- Bootstrap core JavaScript
+    ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
+</head>
+
+<body>
+
+<!-- Fixed navbar -->
+
+
+<!-- Begin page content --><!-- InstanceBeginEditable name="contenido" -->
+<div class="container">
+<div class="row">
+<div class="col-lg-12">
+<span class="text-center">
+<img class="img-responsive center-block" src="imagenes/Tamtilogo.jpg" alt="Renovando">
+</span>
+<hr>
+<h3 class="text-center">Nos estamos renovando para ti.</h3>
+</div>
+</div>
+</div>
+<!-- InstanceEndEditable -->
+
+<div class="footer">
+  <div class="container">
+    <p class="text-muted text-center">Jcastaneda.</p>
+  </div>
+</div>
+</body>
+<!-- InstanceEnd --></html>
